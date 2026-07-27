b0f6f2f72760f8f66fa4b304035bfe349e62b369
Guilherme Compri <guilherme.compri@fresenius-kabi.com>
2026-07-27 15:28:13 +0530

    Corrige a rota inicial no urls.py principal da pasta config

--- core/urls.py
+++ core/urls.py
@@ -2,11 +2,10 @@ from django.contrib import admin
 from django.urls import path
 from django.http import HttpResponse
 
-# Opcional: uma view simples para a raiz "/" não dar Not Found
 def home(request):
     return HttpResponse("<h1>Projeto Django rodando com sucesso no Render!</h1>")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.py), # Mostra essa mensagem na página inicial
+    path('', home),
 ]
