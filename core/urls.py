aa657ef0c03995f350f31391ddf7f430005419c2
Guilherme Compri <guilherme.compri@fresenius-kabi.com>
2026-07-27 15:12:55 +0530

    Adiciona rota inicial e painel admin no urls.py

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,12 @@
-from django.urls import path
-from . import views
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from django.http import HttpResponse
+
+# Opcional: uma view simples para a raiz "/" não dar Not Found
+def home(request):
+    return HttpResponse("<h1>Projeto Django rodando com sucesso no Render!</h1>")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('core.urls')),
+    path('', home), # Mostra essa mensagem na página inicial
 ]
